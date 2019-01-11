92c5eba45f6c566bbc36783c36aedfaabddffb49
Alexander Miehe <alexander.miehe@flaconi.de>
2019-01-11 13:58:03 +0100

    * add new steps for command

--- src/Context/CommandContext.php
+++ src/Context/CommandContext.php
@@ -7,6 +7,8 @@ use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Component\Console\Input\StringInput;
 use Symfony\Component\Console\Output\StreamOutput;
 use Symfony\Component\HttpKernel\KernelInterface;
+use Throwable;
+use Webmozart\Assert\Assert;
 
 /**
  * @author Alexander Miehe <alexander.miehe@flaconi.de>
@@ -18,19 +20,24 @@ class CommandContext implements Context
      */
     private $kernel;
 
+    /**
+     * @var Throwable
+     */
+    private $exception;
+
     public function __construct(KernelInterface $kernel)
     {
         $this->kernel = $kernel;
     }
 
     /**
-     * @When I run command :commandString
+     * @Given I run command :commandString
      *
      * @param string $commandString
      *
      * @throws \Exception
      */
-    public function runCommendInMode(string $commandString): void
+    public function runCommend(string $commandString): void
     {
         $application = new Application($this->kernel);
         $argvInput = new StringInput($commandString);
@@ -40,6 +47,47 @@ class CommandContext implements Context
         if (!$stream) {
             throw new \RuntimeException('stream could not be created.');
         }
-        $command->run($argvInput, new StreamOutput($stream));
+        try {
+            $command->run($argvInput, new StreamOutput($stream));
+        } catch (Throwable $exception) {
+            $this->exception = $exception;
+        }
+    }
+
+    /**
+     * @Given I run command :commandString without fail
+     *
+     * @param string $commandString
+     *
+     * @throws \Exception
+     */
+    public function runCommendWithoutFail(string $commandString): void
+    {
+        $this->runCommend($commandString);
+        $this->shouldNotFailWithAnException();
+    }
+
+    /**
+     * @Then the command should not fail with an exception
+     */
+    public function shouldNotFailWithAnException(): void
+    {
+        Assert::null($this->exception);
+    }
+
+    /**
+     * @Then the command should fail with an exception
+     */
+    public function shouldFailWithAnException(): void
+    {
+        Assert::isInstanceOf($this->exception, Throwable::class);
+    }
+
+    /**
+     * @Then the exception message is :messsage
+     */
+    public function excepetionMessageIs(string $message): void
+    {
+        Assert::eq($message, $this->exception->getMessage());
     }
 }
