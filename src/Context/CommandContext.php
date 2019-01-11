2daf6e7099d3f1ea0ff6ab1b5add53499a64522a
Alexander Miehe <alexander.miehe@flaconi.de>
2019-01-11 11:43:51 +0100

    * fix namespace

--- src/Context/CommandContext.php
+++ src/Context/CommandContext.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types = 1);
 
-namespace App\Behat;
+namespace Flaconi\Behat\Context;
 
 use Behat\Behat\Context\Context;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
