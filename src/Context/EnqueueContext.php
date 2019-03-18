f245ccfcf8e9a58d804c0a36e9e378dbfe9a1a73
Alexander Miehe <alexander.miehe@gmx.de>
2019-03-18 09:17:53 +0100

    Allow mulitple context (#2)
    * update to latest flaconi coding standard
    * allow multiple contexts in enqueue context

--- src/Context/EnqueueContext.php
+++ src/Context/EnqueueContext.php
@@ -7,11 +7,15 @@ use Behat\Gherkin\Node\PyStringNode;
 use Enqueue\Fs\FsDestination;
 use Exception;
 use Interop\Queue\Context as QueueContext;
+use Interop\Queue\InvalidDestinationException;
+use Interop\Queue\InvalidMessageException;
 use Symfony\Component\Filesystem\Exception\FileNotFoundException;
 use Webmozart\Assert\Assert;
+use function array_key_exists;
 use function explode;
 use function file_get_contents;
 use function json_decode;
+use function sprintf;
 use function substr_count;
 use function trim;
 
@@ -21,14 +25,14 @@ use function trim;
 final class EnqueueContext implements Context
 {
     /**
-     * @var QueueContext
+     * @var array<QueueContext>
      */
     private $context;
 
     /**
-     * @param QueueContext $context
+     * @param array<QueueContext> $context
      */
-    public function __construct(QueueContext $context)
+    public function __construct(array $context)
     {
         $this->context = $context;
     }
@@ -40,33 +44,45 @@ final class EnqueueContext implements Context
      */
     public function purgeTopic(string $topicName): void
     {
-        $this->context->purgeQueue($this->context->createQueue($topicName));
+        foreach ($this->context as $context) {
+            $context->purgeQueue($context->createQueue($topicName));
+        }
     }
 
     /**
      * @param string       $topicName
+     * @param string       $contextName
      * @param PyStringNode $message
      *
-     * @throws Exception
+     * @throws \Interop\Queue\Exception
+     * @throws InvalidDestinationException
+     * @throws InvalidMessageException
      *
-     * @Given I push to :topic a message:
+     * @Given I push to :topic in context :contextName a message:
      */
-    public function pushMessageToTopic(string $topicName, PyStringNode $message): void
+    public function pushMessageToTopicInContext(string $topicName, string $contextName, PyStringNode $message): void
     {
-        $psrMessage = $this->context->createMessage('', json_decode($message->getRaw(), true));
-        $topic = $this->context->createQueue($topicName);
-        $this->context->createProducer()->send($topic, $psrMessage);
+        $context = $this->getContext($contextName);
+
+        $psrMessage = $context->createMessage('', json_decode($message->getRaw(), true));
+        $topic = $context->createQueue($topicName);
+        $context->createProducer()->send($topic, $psrMessage);
     }
 
     /**
      * @param string $topicName
+     * @param string $contextName
      * @param int    $count
      *
-     * @Given the count of topic :topic should be :count
+     * @throws Exception
+     *
+     * @Given the count of topic :topic in context :contextName should be :count
      */
-    public function topicCountShouldBe(string $topicName, int $count): void
+    public function topicCountInContextShouldBe(string $topicName, string $contextName, int $count): void
     {
-        $topic = $this->context->createQueue($topicName);
+        $context = $this->getContext($contextName);
+
+        $topic = $context->createQueue($topicName);
 
         if (!$topic instanceof FsDestination) {
             throw new Exception('Topic count is only implemented for now with support for the package "enqueue/fs".');
@@ -79,13 +95,18 @@ final class EnqueueContext implements Context
 
     /**
      * @param string       $topicName
+     * @param string       $contextName
      * @param PyStringNode $message
      *
-     * @Given the topic :topicName should have a message:
+     * @throws Exception
+     *
+     * @Given the topic :topicName in :contextName should have a message:
      */
-    public function topicShouldHaveAMessage(string $topicName, PyStringNode $message): void
+    public function topicInContextShouldHaveAMessage(string $topicName, string $contextName, PyStringNode $message): void
     {
-        $topic = $this->context->createQueue($topicName);
+        $context = $this->getContext($contextName);
+
+        $topic = $context->createQueue($topicName);
 
         if (!$topic instanceof FsDestination) {
             throw new Exception('Topic count is only implemented for now with support for the package "enqueue/fs".');
@@ -116,5 +137,14 @@ final class EnqueueContext implements Context
         }
 
         return $fileContents;
+    }
+
+    private function getContext(string $contextName): QueueContext
+    {
+        if (array_key_exists($contextName, $this->context)) {
+            return $this->context[$contextName];
+        }
+
+        throw new Exception(sprintf('Context with name "%s" was not found.', $contextName));
     }
 }
